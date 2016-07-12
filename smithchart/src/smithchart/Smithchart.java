fe3ef2ec46fe8f59baba9fe29a7914acf729c3c3
LumiaSun <lumiasun92@outlook.com>
2016-07-12 22:32:23 +0800

    add buttons

--- smithchart/src/smithchart/Smithchart.java
+++ smithchart/src/smithchart/Smithchart.java
@@ -6,11 +6,14 @@
 package smithchart;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+//import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,20 +24,25 @@ public class Smithchart extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        //grid.setHgap(10);
+        //grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        
+        Button one = new Button("1");
+        grid.add(one, 0, 0, 2, 1);
+        
+        //btn.setOnAction(new EventHandler<ActionEvent>() {
             
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        //    @Override
+        //    public void handle(ActionEvent event) {
+        //        System.out.println("Hello World!");
+        //    }
+        //});
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(grid, 300, 250);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
